6601e29e8b4c0c4848bab47567efa816656b99c6
Andres-QA <a.quesada@newstreetdev.com>
2023-10-14 20:00:09 -0600

    delete modules (package.json)

--- server/middlewares/cors.ts
+++ server/middlewares/cors.ts
@@ -1,25 +1,25 @@
-import cors from 'cors';
+// import cors from 'cors';
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234'
-];
+// const ACCEPTED_ORIGINS = [
+//   'http://localhost:8080',
+//   'http://localhost:1234'
+// ];
 
-interface CorsMiddlewareOptions {
-  acceptedOrigins?: string[];
-}
+// interface CorsMiddlewareOptions {
+//   acceptedOrigins?: string[];
+// }
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS }: CorsMiddlewareOptions = {}) =>
-  cors({
-    origin: (origin, callback) => {
-      if (acceptedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
+// export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS }: CorsMiddlewareOptions = {}) =>
+//   cors({
+//     origin: (origin, callback) => {
+//       if (acceptedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
 
-      if (!origin) {
-        return callback(null, true);
-      }
+//       if (!origin) {
+//         return callback(null, true);
+//       }
 
-      return callback(new Error('Not allowed by CORS'));
-    }
-  });
+//       return callback(new Error('Not allowed by CORS'));
+//     }
+//   });
