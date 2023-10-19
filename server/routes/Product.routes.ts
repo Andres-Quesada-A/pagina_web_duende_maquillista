3af2221361be5349b7fb378526c4799793a65aa3
Paúl Rodríguez García <70735615+psluk@users.noreply.github.com>
2023-10-19 10:38:52 -0600

    Product management in API

---
+++ server/routes/Product.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import { MasterController } from "../controllers/MasterController";
+
+const MasterControllerObject = new MasterController();
+
+const ProductRouter = Router();
+
+ProductRouter.get(`/get_product_list/:category`, MasterControllerObject.getProductList);
+ProductRouter.get(`/get_product/:id`, MasterControllerObject.getProduct);
+ProductRouter.post(`/create_product`, MasterControllerObject.createProduct);
+ProductRouter.put(`/edit_product`, MasterControllerObject.editProduct);
+ProductRouter.delete(`/delete_product/:id`, MasterControllerObject.deleteProduct);
+
+export default ProductRouter;
