5f59db0f9a321d93f385a117dcb88cf8f807931a
Luis Fernando <luisfmp@estudiantec.cr>
2023-10-19 16:56:56 -0600

    change of variable names

--- server/routes/ImageCategory.routes.ts
+++ server/routes/ImageCategory.routes.ts
@@ -4,14 +4,14 @@ import {MasterController} from '../controllers/MasterController'
 
 const MasterControllerObject = new MasterController()
 
-const UserRouter = Router()
+const ImageSubcategoryRouter = Router()
 //get -> query
 //post -> body
 //put -> body
 //delete ->query
-UserRouter.get(`/get_category`, MasterControllerObject.getImageCategoryList)
-UserRouter.post(`/register_category`, MasterControllerObject.createImageCategory)
-UserRouter.put(`/edit_category`, MasterControllerObject.editImageCategory)
-UserRouter.delete(`/delete/:category`, MasterControllerObject.deleteImageCategory)
+ImageSubcategoryRouter.get(`/get_category`, MasterControllerObject.getImageCategoryList)
+ImageSubcategoryRouter.post(`/create_category`, MasterControllerObject.createImageCategory)
+ImageSubcategoryRouter.put(`/edit_category`, MasterControllerObject.editImageCategory)
+ImageSubcategoryRouter.delete(`/delete/:category`, MasterControllerObject.deleteImageCategory)
 
-export default UserRouter
+export default ImageSubcategoryRouter
--- server/routes/ImageSubcategory.routes.ts
+++ server/routes/ImageSubcategory.routes.ts
@@ -4,14 +4,14 @@ import {MasterController} from '../controllers/MasterController'
 
 const MasterControllerObject = new MasterController()
 
-const UserRouter = Router()
+const ImageCategoryRouter = Router()
 //get -> query
 //post -> body
 //put -> body
 //delete ->query
-UserRouter.get(`/get_subcategory/:category`, MasterControllerObject.getImageSubcategoryList)
-UserRouter.post(`/register_subcategory`, MasterControllerObject.createImageSubcategory)
-UserRouter.put(`/edit_subcategory`, MasterControllerObject.editImageSubcategory)
-UserRouter.delete(`/delete/:category/:subcategory`, MasterControllerObject.deleteImageSubcategory)
+ImageCategoryRouter.get(`/get_subcategory/:category`, MasterControllerObject.getImageSubcategoryList)
+ImageCategoryRouter.post(`/create_subcategory`, MasterControllerObject.createImageSubcategory)
+ImageCategoryRouter.put(`/edit_subcategory`, MasterControllerObject.editImageSubcategory)
+ImageCategoryRouter.delete(`/delete/:category/:subcategory`, MasterControllerObject.deleteImageSubcategory)
 
-export default UserRouter
+export default ImageCategoryRouter
