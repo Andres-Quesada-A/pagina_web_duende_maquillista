b668ee8728e8b286fe84929e45bded58f8a95b4f
Luis Fernando <luisfmp@estudiantec.cr>
2023-10-23 15:42:32 -0600

    try on category front

--- client/src/Pages/Category/ConfigureCategory.jsx
+++ client/src/Pages/Category/ConfigureCategory.jsx
@@ -5,13 +5,44 @@ import DeleteCategory from "../../components/FormCategory/deleteCategory";
 import AddSubcategory from "../../components/FormSubcategory/AddSubcategory";
 import DeleteSubcategory from "../../components/FormSubcategory/DeleteSubcategory";
 import EditSubcategory from "../../components/FormSubcategory/EditSubcategory";
+import axios from 'axios';
 
 function ConfigureCategory() {
+  const [data, setData] = useState([]);
+  const [categoryFilter, setcategoryFilter] = useState({});
   const [categories, setCategories] = useState([]);
   const [subcategories, setSubcategories] = useState([]);
+  
+
   useEffect(() => {
     // Aqui se deben recuperar las categorias y subcategorias
-  });
+
+    const apiCategoryURL = 'http://localhost:1234/api/get_category'
+    
+    axios.get(apiCategoryURL).then((response) => {
+      const dataCategories = response.data
+      console.log(response)
+      setData(dataCategories);
+      setCategories(dataCategories.map(item => ({"description": item.category})))
+    }).catch((error) => {
+      toast.error("No se logro recuperar las categorias", messageSettings);
+    })
+
+
+  }, []);
+
+  const handleCategoryFilter = (e) => {
+
+    setFilters({ ...filters, [e.target.id]: e.target.value });
+    
+    setSubcategory();
+  };
+
+  const handleChangeSubcategoryFilters = (e) => {
+
+    setSubcategoryfilters({ ...Subcategoryfilters, [e.target.id]: e.target.checked });
+  };
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center mt-16 py-14">
       <header className="w-full max-w-4xl">
@@ -26,12 +57,12 @@ function ConfigureCategory() {
         <EditCategory Categories={categories} />
         <EditSubcategory
           Categories={categories}
-          SubCategories={subcategories}
+          SubCategories={data}
         />
         <DeleteCategory Categories={categories} />
         <DeleteSubcategory
           Categories={categories}
-          SubCategories={subcategories}
+          SubCategories={data}
         />
       </section>
       <button
