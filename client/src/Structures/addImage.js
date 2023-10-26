1e7e82ce1fb6a0b8fa120ea718b865e93f418842
fabiansj0028 <fabiansj0028@gmail.com>
2023-10-25 19:37:04 -0600

    Add image form page

---
+++ client/src/Structures/addImage.js
@@ -0,0 +1,23 @@
+export const CreateImage = [
+    {
+      label: "Nombre",
+      type: "text",
+      id: "name",
+      placeholder: "Nombre",
+      required: true,
+    },
+    {
+      label: "Descripción",
+      type: "textarea",
+      id: "description",
+      placeholder: "Descripción",
+      required: true,
+    },
+    {
+      label: "Tags",
+      type: "text",
+      id: "tags",
+      placeholder: "#tag1 #tag2 #tag3...",
+      required: true,
+    }
+  ];
