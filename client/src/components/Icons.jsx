2a4e6e9baaa92a3f93578f442e54351792629b35
Luis Fernando <luisfmp@estudiantec.cr>
2023-10-25 18:19:07 -0600

    new icons

--- client/src/components/Icons.jsx
+++ client/src/components/Icons.jsx
@@ -49,6 +49,56 @@ export const Plane = (props) => {
   );
 };
 
+export function TrashIcon(props) {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M18 6l-.8 12.013c-.071 1.052-.106 1.578-.333 1.977a2 2 0 01-.866.81c-.413.2-.94.2-1.995.2H9.994c-1.055 0-1.582 0-1.995-.2a2 2 0 01-.866-.81c-.227-.399-.262-.925-.332-1.977L6 6M4 6h16m-4 0l-.27-.812c-.263-.787-.394-1.18-.637-1.471a2 2 0 00-.803-.578C13.938 3 13.524 3 12.694 3h-1.388c-.829 0-1.244 0-1.596.139a2 2 0 00-.803.578c-.243.29-.374.684-.636 1.471L8 6m6 4v7m-4-7v7"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export function PencilIcon(props) {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="0 0 192 192"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M26.252 131.616L22 170l38.383-4.252L170 56.073v-12.93L148.857 22h-12.93zm98.164-96.96l31.671 31.67"
+        stroke="currentColor"
+        strokeWidth={12}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M117.733 73.917l-52.882 52.882"
+        stroke="currentColor"
+        strokeWidth={12}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={5}
+      />
+    </svg>
+  )
+}
+
 export function CloseIcon(props) {
   return (
     <svg
