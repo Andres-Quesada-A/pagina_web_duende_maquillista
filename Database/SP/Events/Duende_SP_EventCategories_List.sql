3a8a522337c90119cc82a4e2714a62030034a897
Luis Fernando <luisfmp@estudiantec.cr>
2023-11-20 18:30:33 -0600

    SP eventCategories List

---
+++ Database/SP/Events/Duende_SP_EventCategories_List.sql
@@ -0,0 +1,59 @@
+﻿--------------------------------------------------------------------------
+-- Autor:       Luis Fernando Molina
+-- Fecha:       2023-09-03
+-- Descripci�n: listamos los Eventos en el rango de fechas
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[Duende_SP_EventCategories_List]
+    -- Par�metros,
+AS
+BEGIN
+    SET NOCOUNT ON;         -- No retorna metadatos
+
+	--Variables
+
+    -- CONTROL DE ERRORES
+    DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+    DECLARE @transactionBegun BIT = 0;
+
+    BEGIN TRY
+        
+		SELECT id, description
+		FROM [dbo].[EventCategories]
+		WHERE deleted = 0
+
+
+    END TRY
+    BEGIN CATCH
+
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transactionBegun = 1
+        BEGIN
+            ROLLBACK;
+        END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+            -- Si no es un error personalizado, se registra el error
+            INSERT INTO [dbo].[Errors]
+            VALUES (
+                SUSER_NAME(),
+                ERROR_NUMBER(),
+                ERROR_STATE(),
+                ERROR_SEVERITY(),
+                ERROR_LINE(),
+                ERROR_PROCEDURE(),
+                ERROR_MESSAGE(),
+                GETUTCDATE()
+            );
+        END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+
+    END CATCH;
+END;
